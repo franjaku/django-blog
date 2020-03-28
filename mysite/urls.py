1c5400f54d20b6b71be5edd8bd4b58e3c9539c58
franjaku <kulyckyf@umich.edu>
2020-03-28 15:33:48 -0700

    blogging url

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('blogging/', include('blogging.urls')),
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
-    path('', include('blogging.urls'))
 ]
