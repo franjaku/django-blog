3cbc6236a3411fb0fc62a11b983d20d4b34557c1
franjaku <kulyckyf@umich.edu>
2020-03-29 13:08:07 -0700

    post inline

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class PostInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        PostInline,
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts', )
+    readonly_fields = ('posts', )
+    # inlines = [
+    #     PostInline,
+    # ]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
