9da7dd0525519cfa378dcca834b62297f046cb46
franjaku <kulyckyf@umich.edu>
2020-04-08 23:30:53 -0700

    RESTied blogging app

--- blogging/views.py
+++ blogging/views.py
@@ -1,10 +1,50 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404  # , HttpResponseRedirect
-from blogging.models import Post
+from blogging.models import Post, Category
 from blogging.forms import PostForm
-# from django.template import loader
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from blogging.serializers import UserSerializer, GroupSerializer, PostSerializer, CategorySerializer
 
 
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Users to be viewed or edited
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows posts to be viewed or edited
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+#  Everything below is funtion based views
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
 
